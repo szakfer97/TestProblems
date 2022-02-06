d6e9dd61a00b1ff6e996e4042acb666d843ae3fb
Szakacsi Ferenc <szakfer97@gmail.com>
2022-02-06 20:00:25 +0200

    TestProblems

--- src/App.java
+++ src/App.java
@@ -6,7 +6,9 @@ public class App {
         System.out.println("This programme will solve commonly seen test problems");
         Scanner keyboard = new Scanner(System.in);
         int num = keyboard.nextInt();
+        String st = keyboard.nextLine();
         FizzBuzz(num);
+        RomanToInt(st);
         keyboard.close();
     }
 
@@ -22,5 +24,39 @@ public class App {
                 System.out.println(i);
             }
         }
+    }
+
+    public static int RomanToInt(String S) {
+        int ans = 0, num = 0;
+        for (int i = S.length() - 1; i >= 0; i--) {
+            switch (S.charAt(i)) {
+                case 'I':
+                    num = 1;
+                    break;
+                case 'V':
+                    num = 5;
+                    break;
+                case 'X':
+                    num = 10;
+                    break;
+                case 'L':
+                    num = 50;
+                    break;
+                case 'C':
+                    num = 100;
+                    break;
+                case 'D':
+                    num = 500;
+                    break;
+                case 'M':
+                    num = 1000;
+                    break;
+            }
+            if (4 * num < ans)
+                ans -= num;
+            else
+                ans += num;
+        }
+        return ans;
     }
 }
