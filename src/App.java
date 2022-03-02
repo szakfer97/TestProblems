cc703e3a377278bd6e1992f74abeda3937e007b6
Szakacsi Ferenc <szakfer97@gmail.com>
2022-03-02 18:02:37 +0200

    TestProblems

--- src/App.java
+++ src/App.java
@@ -10,6 +10,7 @@ public class App {
         int num = keyboard.nextInt();
         String st = keyboard.nextLine();
         FizzBuzz(num);
+        LongestPalSubstr(st);
         RomanToInt(st);
         LongestSubstringWithNoRepeatingCharacters(st);
         keyboard.close();
@@ -27,6 +28,37 @@ public class App {
                 System.out.println(i);
             }
         }
+    }
+
+    static int LongestPalSubstr(String str) {
+        int n = str.length();
+        if (n < 2) {
+            return n;
+        }
+        int maxLength = 1, start = 0;
+        int low, high;
+        for (int i = 0; i < n; i++) {
+            low = i - 1;
+            high = i + 1;
+            while (high < n && str.charAt(high) == str.charAt(i)) // increment 'high'
+                high++;
+
+            while (low >= 0 && str.charAt(low) == str.charAt(i)) // decrement 'low'
+                low--;
+
+            while (low >= 0 && high < n && str.charAt(low) == str.charAt(high)) {
+                low--;
+                high++;
+            }
+            int length = high - low - 1;
+            if (maxLength < length) {
+                maxLength = length;
+                start = low + 1;
+            }
+        }
+        System.out.print("Longest palindrome substring is: ");
+        System.out.println(str.substring(start, start + maxLength));
+        return maxLength;
     }
 
     private static int RomanToInt(String S) {
@@ -55,10 +87,11 @@ public class App {
                     num = 1000;
                     break;
             }
-            if (4 * num < ans)
+            if (4 * num < ans) {
                 ans -= num;
-            else
+            } else {
                 ans += num;
+            }
         }
         return ans;
     }
